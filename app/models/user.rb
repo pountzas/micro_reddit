a5d4d01dd3321a6e8150fae003b178c1e1c0353a
pountzas <nikos@pountzas.gr>
2021-05-04 20:26:30 +0300

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 4 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, uniqueness: true
 end
