187c15febe6b1efd19adf019c8adc46c155e465e
pountzas <nikos@pountzas.gr>
2021-05-04 23:50:55 +0300

    add plural to posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4, maximum: 25 }
   validates :body, presence: true, length: { minimum: 4 }
 
-  has_many :comment, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 5 }
   validates :email, presence: true, uniqueness: true
 
-  has_many :post, dependent: :destroy
-  has_many :comment, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
